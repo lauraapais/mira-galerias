d054b51de888c3e2bb97d48c5d583f99f1fcf901
Laura Pais <pais.laura@gmail.com>
2025-01-15 16:59:18 +0000

    ++

--- assets/js/index_3.js
+++ assets/js/index_3.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all elements with the 'carousel' class
-    var sliders = document.querySelectorAll('.carousel');
+  var sliders = document.querySelectorAll('.carousel');
 
-    // Loop through each slider and customize it
-    sliders.forEach(function (slider) {
-      var flkty = new Flickity(slider, {
-        wrapAround: true,
-        imagesLoaded: true,
-        freeScroll: true,
-      });
-
-      var prevButton = slider.querySelector('.flickity-prev-next-button.previous');
-      var nextButton = slider.querySelector('.flickity-prev-next-button.next');
-
-      if (prevButton && nextButton) {
-        prevButton.innerHTML = '<span class="custom-prev-button">Previous</span>';
-        nextButton.innerHTML = '<span class="custom-next-button">Next</span>';
-      }
+  sliders.forEach(function (slider) {
+    var flkty = new Flickity(slider, {
+      wrapAround: true,
+      imagesLoaded: true,
+      freeScroll: true,
+      cellAlign: 'left',
+      contain: true,
+      cellSpacing: 10 // Adjust this value to set the spacing between images
     });
+
+    var prevButton = slider.querySelector('.flickity-prev-next-button.previous');
+    var nextButton = slider.querySelector('.flickity-prev-next-button.next');
+
+    if (prevButton && nextButton) {
+      prevButton.innerHTML = '<span class="custom-prev-button">Previous</span>';
+      nextButton.innerHTML = '<span class="custom-next-button">Next</span>';
+    }
   });
+});
